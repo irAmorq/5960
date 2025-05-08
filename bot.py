b034834e9c278dc2b36c0856f6443a69ee10f99c
irAmorq <lines13801@gmail.com>
2025-05-08 22:34:52 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,3 +13,15 @@ if __name__ == "__main__":
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
+import threading
+import http.server
+import socketserver
+
+def fake_server():
+    PORT = 8080
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        httpd.serve_forever()
+
+threading.Thread(target=fake_server, daemon=True).start()
